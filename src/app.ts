eb9b3082a88718dafa0180ec689cd0b16bf3bdfb
Fahim Orko <fahimorko4122@gmail.com>
2025-10-19 00:12:23 +0300

    adding app server etc

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import express, { Request, Response } from "express";
 
 const app = express();
 
+// Middleware
+app.use(express.json()); // Parse JSON requests
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello TypeScript + Express!");
 });
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,21 @@
 import http from "http";
 import app from "./app";
-import dotenv from "dotenv";
-dotenv.config();
+import { port } from "./config/secrets";
+import connectDB from "./config/db";
 
-const PORT = process.env.PORT || 3000;
+const PORT = port;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Server listening to http//localhost:${PORT}`);
+async function main() {
+  await connectDB();
+
+  server.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+  });
+}
+
+main().catch((error) => {
+  console.error("Failed to start server:", error);
+  process.exit(1);
 });
